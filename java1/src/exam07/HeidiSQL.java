86e59030d4ebb8208936d9711a6008bcc6d3324b
wj-henry <hacksharp@naver.com>
2017-07-30 16:18:27 +0900

    HeidiSQL 수업시간에 했던 명령어

---
+++ java1/src/exam07/HeidiSQL.java
@@ -0,0 +1,16 @@
+package exam07;
+
+public class HeidiSQL {
+
+//	create table user_info(
+//			uinum int auto_increment primary key,
+//			uiname varchar(100) not null,
+//			uiage int not null,
+//			uiaddress varchar(300)
+//			);
+//
+//			insert into user_info(uiname, uiage, uiaddress)
+//			values('Ryan',30,'Seoul');
+//
+//			select * from user_info;
+}
